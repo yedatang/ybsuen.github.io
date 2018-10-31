067fc48cb7af740c6696f8837d99f1658f22dfff
Bernard Suen <bsysin@gmail.com>
2018-10-31 23:49:53 +0800

    Update Lesson 7
    Update Lesson 7

--- com5961/lesson7-with-Airtable-Integration/scriptlib2.js
+++ com5961/lesson7-with-Airtable-Integration/scriptlib2.js
@@ -47,16 +47,15 @@ $(document).ready(function(){
      $("button#roll_up").click(function() {
          var items = [];
          var i = 0;
-         var airtable_read_endpoint = "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
+         var airtable_read_endpoint =
+         "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Category?api_key=keyTcsTzckqyBTlk8";
          var dataSet = [];
          $.getJSON(airtable_read_endpoint, function(result) {
                 $.each(result.records, function(key,value) {
                     items = [];
                         items.push(value.fields.Name);
-                        items.push(value.fields.Completed);
-                        items.push(value.fields.Time_Estimate);
-                        items.push(value.fields.Weight_Factor);
-                        items.push(value.fields.converted);
+                        items.push(value.fields.Task);
+                        items.push(value.fields.Total_Entries);
                         dataSet.push(items);
                  }); // end .each
               $('#example').DataTable( {
@@ -65,13 +64,9 @@ $(document).ready(function(){
                   columns: [
                       { title: "Name",
                         defaultContent:""},
-                      { title: "Completed",
+                      { title: "Tasks",
                         defaultContent:"" },
-                      { title: "Time Estimated",
-                        defaultContent:""},
-                      { title: "Weight Factor",
-                          defaultContent:""},
-                      { title: "Converted",
+                      { title: "Total Entries",
                         defaultContent:""},
                   ]
               } );
