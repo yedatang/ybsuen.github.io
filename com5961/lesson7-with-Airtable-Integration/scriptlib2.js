657927328aadaf4f37a43c9a5d10adb6b3230a72
Bernard Suen <bsysin@gmail.com>
2018-10-31 23:38:51 +0800

    Update Lesson 7
    Update Lesson 7

--- com5961/lesson7-with-Airtable-Integration/scriptlib2.js
+++ com5961/lesson7-with-Airtable-Integration/scriptlib2.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     $("button#hide_h2").click(function() {
         $("h2").hide(500);
     });
@@ -42,4 +43,39 @@ $(document).ready(function(){
              } );
         }); // end .getJSON
      }); // end button
+
+     $("button#roll_up").click(function() {
+         var items = [];
+         var i = 0;
+         var airtable_read_endpoint = "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
+         var dataSet = [];
+         $.getJSON(airtable_read_endpoint, function(result) {
+                $.each(result.records, function(key,value) {
+                    items = [];
+                        items.push(value.fields.Name);
+                        items.push(value.fields.Completed);
+                        items.push(value.fields.Time_Estimate);
+                        items.push(value.fields.Weight_Factor);
+                        items.push(value.fields.converted);
+                        dataSet.push(items);
+                 }); // end .each
+              $('#example').DataTable( {
+                  data: dataSet,
+                  retrieve: true,
+                  columns: [
+                      { title: "Name",
+                        defaultContent:""},
+                      { title: "Completed",
+                        defaultContent:"" },
+                      { title: "Time Estimated",
+                        defaultContent:""},
+                      { title: "Weight Factor",
+                          defaultContent:""},
+                      { title: "Converted",
+                        defaultContent:""},
+                  ]
+              } );
+         }); // end .getJSON
+      }); // end button
+
 }); // document ready
