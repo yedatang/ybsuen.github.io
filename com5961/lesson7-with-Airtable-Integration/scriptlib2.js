722349850f255eb575751422bb9af8d811dbf85b
Bernard Suen <bsysin@gmail.com>
2018-11-01 10:06:38 +0800

    Update Lesson 7 wiith C3
    Update Lesson 7 wiith C3

--- com5961/lesson7-with-Airtable-Integration/scriptlib2.js
+++ com5961/lesson7-with-Airtable-Integration/scriptlib2.js
@@ -76,26 +76,22 @@ $(document).ready(function(){
                     ] // rmf columns
                 } ); // end dataTable
 
-                var  chart = c3.generate({
-
-                  data: {
-                      json: dataSet,
-                      type: 'bar',
-                      keys: {
-                          x: 'Name',
-                          value: ['Total_Entries'],
-                          }
-                  },
-
-                      axis: {
-                          x: {
-                             type: 'category'
-                          }
-                  }
-                }); /* end c3.generate */
-
-
-
+                var chart = c3.generate({
+                        data: {
+                            columns: [
+                                ['data1', 30, 200, 100, 400, 150, 250],
+                                ['data2', 130, 100, 140, 200, 150, 50]
+                            ],
+                            type: 'bar'
+                        },
+                        bar: {
+                            width: {
+                                ratio: 0.5 // this makes bar width 50% of length between ticks
+                            }
+                            // or
+                            //width: 100 // this makes bar width 100px
+                        }
+                    }); // end c3.generate
 
           }); // end .getJSON
        }); // end button
