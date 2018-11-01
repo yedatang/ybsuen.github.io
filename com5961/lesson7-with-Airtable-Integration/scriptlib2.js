71dcea0c2f87f2d0703ee87b05168292d6841807
Bernard Suen <bsysin@gmail.com>
2018-11-01 09:53:24 +0800

    Update 7 with C3
    Update 7 with C3

--- com5961/lesson7-with-Airtable-Integration/scriptlib2.js
+++ com5961/lesson7-with-Airtable-Integration/scriptlib2.js
@@ -74,25 +74,27 @@ $(document).ready(function(){
                     ] // rmf columns
                 } ); // end dataTable
 
-                var chart = c3.generate({
-                      data: {
-                        json: [
-                          {name: 'www.site1.com', upload: 200, download: 200, total: 400},
-                          {name: 'www.site2.com', upload: 100, download: 300, total: 400},
-                          {name: 'www.site3.com', upload: 300, download: 200, total: 500},
-                          {name: 'www.site4.com', upload: 400, download: 100, total: 500}
-                        ],
-                        keys: {
-                          x: 'name', // it's possible to specify 'x' when category axis
-                          value: ['upload', 'download']
-                        }
-                      },
+                var  chart = c3.generate({
+
+                  data: {
+                      json: dataSet,
+                      type: ctype,
+                      keys: {
+                          x: 'Name',
+                          value: ['Total_Entries'],
+                          }
+                  },
+
                       axis: {
-                        x: {
-                          type: 'category'
-                        }
-                      }
-                 }); // c3.generate
+                          x: {
+                             type: 'category'
+                          }
+                  }
+                }); /* end c3.generate */
+
+
+
+
           }); // end .getJSON
        }); // end button
 
