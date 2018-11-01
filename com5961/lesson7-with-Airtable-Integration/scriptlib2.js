801d4ec3d4e31d11889ee5fbcd998f612e2cdfd9
Bernard Suen <bsysin@gmail.com>
2018-11-01 12:34:05 +0800

    Update Lesson 7 with C3
    Update Lesson 7 with C3

--- com5961/lesson7-with-Airtable-Integration/scriptlib2.js
+++ com5961/lesson7-with-Airtable-Integration/scriptlib2.js
@@ -56,6 +56,44 @@ $(document).ready(function(){
      }); // end button
 
      $("button#roll_up").click(function() {
+       var items = [];
+       var i = 0;
+       var airtable_read_endpoint = "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
+       var dataSet = [];
+       $.getJSON(airtable_read_endpoint, function(result) {
+              $.each(result.records, function(key,value) {
+                  items = [];
+                      items.push(value.fields.Name);
+                      items.push(value.fields.Stage_Desc);
+                      items.push(value.fields.Completed);
+                      items.push(value.fields.Time_Estimate);
+                      items.push(value.fields.Weight_Factor);
+                      items.push(value.fields.converted);
+                      dataSet.push(items);
+                      console.log(items);
+               }); // end .each
+               console.log(dataSet);
+
+            $('#table1').DataTable( {
+                data: dataSet,
+                retrieve: true,
+                columns: [
+                    { title: "Name",
+                      defaultContent:""},
+                    { title: "Stage",
+                        defaultContent:"" },
+                    { title: "Completed",
+                      defaultContent:"" },
+                    { title: "Time Estimated",
+                      defaultContent:""},
+                    { title: "Weight Factor",
+                        defaultContent:""},
+                    { title: "Converted",
+                      defaultContent:""},
+                ]
+            } );
+       }); // end .getJSON
+
          var items = [];
          var i = 0;
          var airtable_read_endpoint =
@@ -70,7 +108,7 @@ $(document).ready(function(){
                         console.log(items);
                  }); // end .each
                  console.log(dataSet);
-                $('#example').DataTable( {
+                $('#table2').DataTable( {
                     data: dataSet,
                     retrieve: true,
                     ordering: false,
