b3c2e4b860b5340701225e715e150a4269ff609d
Bernard Suen <bsysin@gmail.com>
2018-11-01 09:42:02 +0800

    Update 7 with C3 Demo
    Update 7 with C3 Demo

--- com5961/lesson7-with-Airtable-Integration/scriptlib2.js
+++ com5961/lesson7-with-Airtable-Integration/scriptlib2.js
@@ -28,6 +28,7 @@ $(document).ready(function(){
                        console.log(items);
                 }); // end .each
                 console.log(dataSet);
+
              $('#example').DataTable( {
                  data: dataSet,
                  retrieve: true,
@@ -72,6 +73,26 @@ $(document).ready(function(){
                           defaultContent:""},
                     ] // rmf columns
                 } ); // end dataTable
+
+                var chart = c3.generate({
+                      data: {
+                        json: [
+                          {name: 'www.site1.com', upload: 200, download: 200, total: 400},
+                          {name: 'www.site2.com', upload: 100, download: 300, total: 400},
+                          {name: 'www.site3.com', upload: 300, download: 200, total: 500},
+                          {name: 'www.site4.com', upload: 400, download: 100, total: 500}
+                        ],
+                        keys: {
+                          // x: 'name', // it's possible to specify 'x' when category axis
+                          value: ['upload', 'download']
+                        }
+                      },
+                      axis: {
+                        x: {
+                          // type: 'category'
+                        }
+                      }
+                 }); // c3.generate
           }); // end .getJSON
        }); // end button
 
