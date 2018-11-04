616e3219a88da8d92916c0879030fe429811120d
Bernard Suen <bsysin@gmail.com>
2018-11-04 11:29:16 +0800

    Update Lesson 7
    Update Lesson 7

--- com5961/lesson7-with-Airtable-Integration/leaf-demo.js
+++ com5961/lesson7-with-Airtable-Integration/leaf-demo.js
@@ -23,8 +23,7 @@ var myIcon = L.icon({
 
 for ( var i=0; i < markers.length; ++i )
 {
-// L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
- L.marker( [markers[i].lat, markers[i].lng])
+ L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
   .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
   .addTo( map );
 }
